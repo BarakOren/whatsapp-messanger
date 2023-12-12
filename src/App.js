1ef0ad4512da5a3a584b7f8df3a09c0e9b53266b
BarakOren <barakoren5@gmail.com>
2023-12-12 17:18:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import list from "./places-of-הובלות-בתל-אביב-יפו.json"
 
 function App() {
+  list.map((item) => {
+    console.log(item)
+  })
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {list.map((item, i) => {
+
+      const before = '972' + item.phone.replace("-", "");
+      const phoneNumber2 = before.replace("-", "");
+
+      var text = "אהלן ערב טוב"
+
+
+      var link = `https://api.whatsapp.com/send?phone=${phoneNumber2}&text=${text}&send=1`
+      var link2 = `https://wa.me/${phoneNumber2}?text=${text}`      
+  
+
+      return (
+        <div className='row'>
+        <p>{i + 1}</p>
+        <a href={link2} target="_blank">{link2}</a>
+        </div>
+      )
+    })}
     </div>
   );
 }
